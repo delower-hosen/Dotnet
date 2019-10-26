fd65743447aedd6c70d9cf096550444d75890b69
Delower <delowerhosencuet13@gmail.com>
2019-10-27 01:54:24 +0600

    demo auth service and fluent validation to validate model

---
+++ Dotnet-Core-WebAPI-Demo/Dotnet-Core-WebAPI-Demo/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dotnet_Core_WebAPI_Demo.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
